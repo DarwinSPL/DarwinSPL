e9404ba1031cf72aeaa1c407b5d50e2816966231
Michael Nieke <github@n1eke.de>
2016-11-21 16:57:58 +0100

    loading constraint model

--- plugins/de.darwinspl.variant_generation/src/de/darwinspl/variant_generation/impl/VariantDerivationImpl.java
+++ plugins/de.darwinspl.variant_generation/src/de/darwinspl/variant_generation/impl/VariantDerivationImpl.java
@@ -10,6 +10,7 @@ import de.darwinspl.variant_generation.VariantDerivationFailedException;
 import eu.hyvar.feature.HyFeatureModel;
 import eu.hyvar.feature.configuration.HyConfiguration;
 import eu.hyvar.feature.constraint.HyConstraintModel;
+import eu.hyvar.feature.constraint.util.HyConstraintUtil;
 import eu.hyvar.feature.mapping.HyMapping;
 import eu.hyvar.feature.util.HyFeatureResolverUtil;
 
@@ -29,13 +30,34 @@ public class VariantDerivationImpl implements VariantDerivation {
 	public void deriveVariant(HyFeatureModel featureModel, HyConstraintModel constraints, HyConfiguration configuration,
 			HyMapping mapping, Date date) throws VariantDerivationFailedException {
 		
+		if(configuration==null) {
+			throw new VariantDerivationFailedException("Configuration was null. Cannot derive variant!");
+		}
+		
 		if(featureModel == null) {
 			EObject potentialFeatureModel = EcoreIOUtil.loadAccompanyingModel(configuration, "", HyFeatureResolverUtil.FILE_EXTENSIONS);
 			if(potentialFeatureModel == null || !(potentialFeatureModel instanceof HyFeatureModel))  {
-				throw new VariantDerivationFailedException("");
+				throw new VariantDerivationFailedException("No Feature Model found!");
 			}
+			
+			featureModel = (HyFeatureModel) potentialFeatureModel;
 		}
 		
+		if(constraints == null) {
+			EObject potentialConstraintModel = EcoreIOUtil.loadAccompanyingModel(configuration, "", HyConstraintUtil.CONSTRAINT_MODEL_FILE_EXTENSION_FOR_CONCRETE_SYNTAX);
+			if(potentialConstraintModel == null) {
+				potentialConstraintModel = EcoreIOUtil.loadAccompanyingModel(configuration, "", HyConstraintUtil.CONSTRAINT_MODEL_FILE_EXTENSION_FOR_XMI);
+			}
+			if(potentialConstraintModel==null || !(potentialConstraintModel instanceof HyConstraintModel)) {
+				// TODO logging
+			} 
+			
+			constraints = (HyConstraintModel) potentialConstraintModel;
+		}
+		
+		// get Mapping
+		
+		// Check if config respects FM structure and Constraint (valid at @date)
 		
 		// TODO Auto-generated method stub
 		
