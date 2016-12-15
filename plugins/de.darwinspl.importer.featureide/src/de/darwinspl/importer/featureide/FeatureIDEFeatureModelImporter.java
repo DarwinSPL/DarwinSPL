dbb050e47e284b07e40680119904ecd64dd7e2bc
Michael Nieke <github@n1eke.de>
2016-12-15 13:34:52 +0100

    added common utils plugin

---
+++ plugins/de.darwinspl.importer.featureide/src/de/darwinspl/importer/featureide/FeatureIDEFeatureModelImporter.java
@@ -0,0 +1,14 @@
+package de.darwinspl.importer.featureide;
+
+import de.darwinspl.importer.DarwinSPLFeatureModelImporter;
+import eu.hyvar.feature.HyFeatureModel;
+
+public class FeatureIDEFeatureModelImporter implements DarwinSPLFeatureModelImporter<T> {
+
+	@Override
+	public HyFeatureModel importFeatureModel(T featureModel) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
