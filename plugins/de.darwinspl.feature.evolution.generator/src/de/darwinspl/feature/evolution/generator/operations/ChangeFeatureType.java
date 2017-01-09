07f8344d2b022fa2d92a7f971836640878f94f6d
Michael Nieke <github@n1eke.de>
2017-01-09 14:47:27 +0100

    added change group type and optimized change feature type

--- plugins/de.darwinspl.feature.evolution.generator/src/de/darwinspl/feature/evolution/generator/operations/ChangeFeatureType.java
+++ plugins/de.darwinspl.feature.evolution.generator/src/de/darwinspl/feature/evolution/generator/operations/ChangeFeatureType.java
@@ -5,34 +5,46 @@ import java.util.Date;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 
 import eu.hyvar.evolution.HyEvolutionUtil;
+import eu.hyvar.evolution.HyLinearTemporalElement;
 import eu.hyvar.feature.HyFeature;
 import eu.hyvar.feature.HyFeatureFactory;
 import eu.hyvar.feature.HyFeatureType;
 import eu.hyvar.feature.HyFeatureTypeEnum;
-import eu.hyvar.feature.HyGroupComposition;
 
 public class ChangeFeatureType implements EvolutionOperation {
 
 	protected HyFeature feature;
 	protected Date date;
 	protected HyFeatureTypeEnum oldType;
+	protected HyFeatureTypeEnum newType;
 
+	public ChangeFeatureType(HyFeature feature, HyFeatureTypeEnum newType, Date date) {
+		this(feature, date);
+		this.newType = newType;
+	}
+	
 	public ChangeFeatureType(HyFeature feature, Date date) {
 		this.feature = feature;
 		this.date = date;
 		this.oldType = HyEvolutionUtil.getValidTemporalElement(feature.getTypes(), date).getType();
+		
+		if (oldType.equals(HyFeatureTypeEnum.MANDATORY)) {
+			newType = HyFeatureTypeEnum.OPTIONAL;
+		} else {
+			newType = HyFeatureTypeEnum.MANDATORY;
+		}
 	}
 
 	@Override
 	public void applyOperation() throws EvolutionOperationException {
+		if(newType.equals(oldType)) {
+			return;
+		}
+		
 		HyFeatureType newFeatureType = HyFeatureFactory.eINSTANCE.createHyFeatureType();
 		newFeatureType.setValidSince(date);
-
-		if (oldType.equals(HyFeatureTypeEnum.MANDATORY)) {
-			newFeatureType.setType(HyFeatureTypeEnum.OPTIONAL);
-		} else {
-			newFeatureType.setType(HyFeatureTypeEnum.MANDATORY);
-		}
+		
+		newFeatureType.setType(newType);
 
 		HyFeatureType oldFeatureType = HyEvolutionUtil.getValidTemporalElement(feature.getTypes(), date);
 		oldFeatureType.setValidUntil(date);
@@ -41,7 +53,7 @@ public class ChangeFeatureType implements EvolutionOperation {
 		
 		// Check if old group composition can be deleted
 		if (oldFeatureType.getValidSince().equals(oldFeatureType.getValidUntil())) {
-			HyGroupComposition oldSupersededElement = (HyGroupComposition) oldFeatureType.getSupersededElement();
+			HyLinearTemporalElement oldSupersededElement = oldFeatureType.getSupersededElement();
 			EcoreUtil.delete(oldFeatureType);
 			newFeatureType.setSupersededElement(oldSupersededElement);
 		} else {
