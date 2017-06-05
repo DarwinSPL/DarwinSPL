2c708bf476265bd6336e0b0e861d5f27179345b7
Jeremias Wrensch <j.wrensch@mugs.de>
2017-06-05 17:51:48 +0200

    added feature expression

---
+++ plugins/de.darwinspl.configurator.expression/src/de/darwinspl/configurator/expression/AtomicFeatureExpression.java
@@ -0,0 +1,11 @@
+package de.darwinspl.configurator.expression;
+
+import eu.hyvar.feature.expression.HyFeatureReferenceExpression;
+import eu.hyvar.feature.expression.impl.HyAbstractFeatureReferenceExpressionImpl;
+
+public class AtomicFeatureExpression extends HyAbstractFeatureReferenceExpressionImpl implements HyFeatureReferenceExpression {
+	
+	public AtomicFeatureExpression(){}
+
+}
+
