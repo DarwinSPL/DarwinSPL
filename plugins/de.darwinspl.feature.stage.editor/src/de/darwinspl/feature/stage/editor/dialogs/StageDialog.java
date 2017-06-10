053c2103c69b17b26578c9f67e0b1a7d72a20ea0
AlexMane <a.manegold@tu-bs.de>
2017-06-10 13:00:01 +0200

    Added first working Layout for the Editor

---
+++ plugins/de.darwinspl.feature.stage.editor/src/de/darwinspl/feature/stage/editor/dialogs/StageDialog.java
@@ -0,0 +1,135 @@
+package de.darwinspl.feature.stage.editor.dialogs;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+
+public class StageDialog extends Dialog implements Listener{
+	
+	protected Composite stageButtonGroup;
+	protected Composite roleButtonGroup;
+	protected Group stageGroup;
+	protected Group roleGroup;
+	
+	protected Button addStageButton;
+	protected Button deleteStageButton;
+	protected Button assignRoleButton;
+	protected Button addRoleButton;
+	protected Button deleteRoleButton;
+
+	
+	public StageDialog(Shell parentShell) {
+		super(parentShell);
+	}
+
+	@Override
+	protected boolean isResizable() {
+	    return true;
+	}
+
+
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		//Grid Layout for area
+		Composite container = (Composite) super.createDialogArea(parent);
+	    container.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+	    GridLayout layout = new GridLayout(4, true);
+	    GridData gridData = new GridData();
+		gridData.horizontalSpan = 2;
+		gridData.horizontalAlignment = SWT.FILL;
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.verticalAlignment = SWT.FILL;
+		gridData.grabExcessVerticalSpace = true;
+	    container.setLayout(layout);
+	    
+	    //Groups
+	    stageGroup = new Group(container, SWT.NONE);
+	    stageGroup.setLayout(new GridLayout( 1, false ));
+	    stageGroup.setLayoutData(gridData);
+	    stageGroup.setText("Stages");
+	    
+	    roleGroup = new Group(container,SWT.NONE);
+	    roleGroup.setLayoutData(gridData);
+	    roleGroup.setText("Roles");
+	    roleGroup.setLayout(new GridLayout( 1, false ));
+	    
+	    // Lists for Stages / Roles	    
+	    List stageList = new List(stageGroup, SWT.BORDER | SWT.SINGLE | SWT.V_SCROLL);
+	    stageList.setLayoutData(new GridData());
+	    List roleList = new List(roleGroup, SWT.BORDER | SWT.SINGLE | SWT.V_SCROLL);
+	    roleList.setLayoutData(new GridData());
+	    
+	     // TODO Replace with a function that gets the names of the existing stages
+	    for (int loopIndex = 0; loopIndex < 10; loopIndex++) {
+	        stageList.add("Stage " + loopIndex);
+	        roleList.add("Role " + loopIndex);
+	      }	   
+	    
+	    // Stage Button Group
+	    stageButtonGroup = new Composite(stageGroup,SWT.NONE);
+		RowLayout rowLayout = new RowLayout (SWT.HORIZONTAL);
+		rowLayout.justify = true;
+		stageButtonGroup.setLayout(rowLayout);			
+		
+		//Role Button Group
+	    roleButtonGroup = new Composite(roleGroup, SWT.NONE);
+	    roleButtonGroup.setLayout(rowLayout);	    
+   
+	    // Buttons for stage management
+	    addStageButton = new Button (stageButtonGroup, SWT.NONE);
+		addStageButton.setText("Add");
+		deleteStageButton = new Button (stageButtonGroup, SWT.NONE);
+		deleteStageButton.setText("Delete");
+		
+		// Buttons for role management
+		addRoleButton = new Button (roleButtonGroup, SWT.NONE);
+		addRoleButton.setText("Add");
+		deleteRoleButton = new Button (roleButtonGroup, SWT.NONE);
+		deleteRoleButton.setText("Delete");
+		assignRoleButton = new Button (roleButtonGroup, SWT.NONE);
+		assignRoleButton.setText("Assign");    
+
+
+	    container.pack();
+		return container;
+	}
+
+	  @Override
+	  protected void createButtonsForButtonBar(Composite parent) {
+	    createButton(parent, IDialogConstants.OK_ID, "Finish", true);
+//	    createButton(parent, IDialogConstants.CANCEL_ID,
+//	        IDialogConstants.CANCEL_LABEL, false);
+	  }	
+	
+	// overriding this methods allows you to set the
+	// title of the custom dialog
+	@Override
+	protected void configureShell(Shell newShell) {
+		super.configureShell(newShell);
+		newShell.setText("Stage/Role Management");
+	}
+
+	@Override
+	protected Point getInitialSize() {
+		return new Point(400, 340);
+	}
+
+	@Override
+	public void handleEvent(Event event) {
+	}		
+
+}
--- plugins/de.darwinspl.feature.stage.editor/src/de/darwinspl/feature/stage/editor/editor/SmStageModelEditor.java
+++ plugins/de.darwinspl.feature.stage.editor/src/de/darwinspl/feature/stage/editor/editor/SmStageModelEditor.java
@@ -1,27 +1,101 @@
 package de.darwinspl.feature.stage.editor.editor;
 
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.gef.DefaultEditDomain;
-import org.eclipse.gef.palette.PaletteRoot;
 
-import de.darwinspl.feature.stage.base.editor.SmFeatureModelViewer;
+import java.util.Date;
 
-public class SmStageModelEditor extends SmFeatureModelViewer {
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+
+import de.darwinspl.feature.stage.editor.dialogs.StageDialog;
+import eu.hyvar.feature.graphical.base.dialogs.DateDialog;
+import eu.hyvar.feature.graphical.base.editparts.HyFeatureModelEditPart;
+import eu.hyvar.feature.graphical.editor.editor.HyGraphicalFeatureModelEditor;
+
+
+
+public class SmStageModelEditor extends HyGraphicalFeatureModelEditor {
 	
-	public SmStageModelEditor() {
-	    setEditDomain(new DefaultEditDomain(this));
-	  }
-	   
-	  @Override
-	  protected PaletteRoot getPaletteRoot() {
-	    // TODO Auto-generated method stub
-	    return null;
-	  }
-	 
-	  @Override
-	  public void doSave(IProgressMonitor monitor) {
-	    // TODO Auto-generated method stub
-	  }
+	protected Combo stageCombo;
+	protected Button stageAssignButton;
+	protected Button stageManagementButton;
+	protected Composite comboGroup;
+	
+	
+	
+	
+	/**
+	 * Creation of the Buttons/Combobox for Stage control in the editor
+	 */
+	public void createStageControl(Composite parent){
+		//Groups for positioning
+		buttonGroup = new Composite(parent,SWT.NONE);
+		RowLayout rowLayout = new RowLayout (SWT.HORIZONTAL);
+		rowLayout.justify = true;
+		buttonGroup.setLayout(rowLayout);		
+		comboGroup = new Composite(buttonGroup, SWT.NONE);
+		RowLayout columnLayout = new RowLayout(SWT.VERTICAL);	
+		columnLayout.justify = true;
+		comboGroup.setLayout(columnLayout);
+		
+		
+		// Combo box for stage Selection
+		stageCombo = new Combo(comboGroup, SWT.NONE);		
+		//Example;
+		String items[] = { "Stage One", "Stage Two"};
+		stageCombo.setItems(items);
+		
+		//Button for Stage Management, Creation/Deletion
+		stageManagementButton = new Button (comboGroup, SWT.NONE);
+		stageManagementButton.setText("Manage Stages/Roles");
+		
+		
+		//Button for Assignment
+		stageAssignButton = new Button(buttonGroup, SWT.TOGGLE);
+		stageAssignButton.setText("Assign Stage");
+		
+		
+	}
+	
+	
+	
+	/**
+	 * New Listener for Stage Buttons
+	 */
+	public void registerStageControlListeners(){
+		// Left button to select an individual date
+		stageManagementButton.addListener(SWT.Selection, new Listener(){
+			public void handleEvent(Event event){
+				StageDialog dialog = new StageDialog(getEditorSite().getShell());
+				dialog.open();
+				if(dialog.getReturnCode()==0){					
+				
+				}
+			}
+		});
+
+	}
+	
+	
+	/** Function to overwrite from DwGraphicalFeatureModelViewer
+	 *  This function adds everything to the layout
+	 */
+	public void createPartControl(Composite parent){
+		parent.setLayout(new GridLayout(2, false));
+		createEditor(parent);
+		createSliderControl(parent);
+		// ##### New Create ######
+		createStageControl(parent);
+		
+		registerControlListeners();
+		registerStageControlListeners();
+		((HyFeatureModelEditPart)getGraphicalViewer().getContents()).refresh();
+	}
 	
 
 }
