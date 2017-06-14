4e0debbb42b4fed32f74555dc43ca4ce0a85848b
Gil Engel <g.engel@tu-bs.de>
2017-06-14 12:38:34 +0200

    Add functionallity to open existing files with the action bar buttons

--- plugins/de.darwinspl.feature.graphical.editor/src/de/darwinspl/feature/graphical/editor/commands/files/DwCreateValidityModelCommand.java
+++ plugins/de.darwinspl.feature.graphical.editor/src/de/darwinspl/feature/graphical/editor/commands/files/DwCreateValidityModelCommand.java
@@ -2,7 +2,7 @@ package de.darwinspl.feature.graphical.editor.commands.files;
 
 import eu.hyvar.context.contextValidity.util.HyValidityModelUtil;
 
-public class DwCreateValidityModelCommand extends DwCreateFileCommand{
+public class DwCreateValidityModelCommand extends DwCreateOrOpenFileCommand{
 
 	@Override
 	protected String getExtension() {
