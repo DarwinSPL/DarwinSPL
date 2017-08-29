45ffb8659a1daa71c3c58eee7fbb95f342c11e67
Michael Nieke <github@n1eke.de>
2017-08-29 11:21:16 +0200

    Updated DarwinSPL configurator to be compatible with newest DeltaEcore
    variant generation

--- plugins/de.darwinspl.feature.graphical.configurator/src/de/darwinspl/feature/graphical/configurator/editor/listeners/DwDeriveVariantListener.java
+++ plugins/de.darwinspl.feature.graphical.configurator/src/de/darwinspl/feature/graphical/configurator/editor/listeners/DwDeriveVariantListener.java
@@ -1,10 +1,27 @@
 package de.darwinspl.feature.graphical.configurator.editor.listeners;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import org.deltaecore.common.eclipse.controls.DEVariantComposite;
+import org.deltaecore.feature.DEFeatureModel;
+import org.deltaecore.feature.configuration.DEConfiguration;
+import org.deltaecore.feature.configuration.util.DEConfigurationIOUtil;
+import org.deltaecore.feature.graphical.configurator.components.DEGenerateVariantDialog;
+import org.deltaecore.feature.graphical.configurator.components.DEVariantGeneratorsComposite;
+import org.deltaecore.feature.util.DEFeatureIOUtil;
+//import org.deltaecore.feature.graphical.configurator.editor.DEConfiguratorGraphicalEditor.ValidateConfigurationResult;
+import org.deltaecore.feature.variant.DEVariantGenerator;
+import org.deltaecore.suite.mapping.DEMappingModel;
+import org.deltaecore.suite.mapping.util.DEMappingIOUtil;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -12,18 +29,20 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Shell;
 
 import de.christophseidl.util.ecore.EcoreIOUtil;
-import de.darwinspl.feature.configuration.variantgeneration.DwConfigurationVariantCreator;
-import de.darwinspl.feature.graphical.configurator.composites.DwVariantComposite;
-import de.darwinspl.feature.graphical.configurator.dialogs.DwVariantDerivationDialog;
 import de.darwinspl.feature.graphical.configurator.editor.DwFeatureModelConfiguratorEditor;
 import eu.hyvar.feature.HyFeatureModel;
 import eu.hyvar.feature.configuration.HyConfiguration;
+import eu.hyvar.feature.exporter.hfm_exporter.HFMConfigurationExporter;
+import eu.hyvar.feature.exporter.hfm_exporter.HFMExporter;
+import eu.hyvar.feature.exporter.hfm_exporter.HFMMappingExporter;
 import eu.hyvar.feature.mapping.HyMappingModel;
 import eu.hyvar.feature.mapping.util.HyMappingModelUtil;
+import eu.hyvar.feature.util.HyFeatureModelWellFormednessException;
 
 public class DwDeriveVariantListener extends SelectionAdapter {
 
 	private DwFeatureModelConfiguratorEditor configurator;
+	private IFolder tempDeltaEcoreFolder;
 
 	public DwDeriveVariantListener(DwFeatureModelConfiguratorEditor configurator) {
 		super();
@@ -49,34 +68,138 @@ public class DwDeriveVariantListener extends SelectionAdapter {
 		
 		HyMappingModel mapping = (HyMappingModel) loadedObject;
 
-		DwConfigurationVariantCreator variantCreator = new DwConfigurationVariantCreator();
+//		DwConfigurationVariantCreator variantCreator = new DwConfigurationVariantCreator();
 
 
 		Date date = configurator.getCurrentSelectedDate();
 
+		
+		final String failureMessageTitle = "Variant Generation Failed!";
+		
+		
+		// TODO Validate selected configuration
+//		ValidateConfigurationResult validateConfigurationResult = doValidateConfiguration();
+		
+//		if (!validateConfigurationResult.getIsValid()) {
+//			MessageDialog.openError(shell, failureMessageTitle, "The configuration is invalid: " + validateConfigurationResult.getErrorMessage());
+//			return;
+//		}
+		
 		try {
-			DwVariantDerivationDialog dialog = new DwVariantDerivationDialog(shell);
+			
+			DEGenerateVariantDialog dialog = new DEGenerateVariantDialog(shell);
 			int result = dialog.open();
-
+			
 			if (result == Dialog.OK) {
-				DwVariantComposite variantComposite = dialog.getVariantComposite();
+				DEVariantGeneratorsComposite generatorsComposite = dialog.getGeneratorsComposite();
+				DEVariantGenerator variantGenerator = generatorsComposite.getVariantGenerator();
+				
+				if (variantGenerator == null) {
+					MessageDialog.openError(shell, failureMessageTitle, "No variant generator selected!");
+					return;
+				}
+				
+				DEVariantComposite variantComposite = dialog.getVariantComposite();
 				IFolder variantFolder = variantComposite.getFolder();
-
-				//variantCreator.createAndSaveVariantFromConfiguration(featureModel, configuration, mapping, date, variantFolder);
-
+				
+				if (variantFolder == null) {
+					MessageDialog.openError(shell, failureMessageTitle, "No variant folder selected!");
+					return;
+				}
+				
+				
+				List<EObject> deltaEcoreModels = getDeltaEcoreModels(featureModel, configuration, mapping, date);
+				
+				DEConfiguration deConfiguration = null;
+				
+				for(EObject eObject : deltaEcoreModels) {
+					if(eObject instanceof DEConfiguration) {
+						deConfiguration = (DEConfiguration) eObject;
+						break;
+					}
+				}
+				
+				if(deConfiguration == null) {
+					throw new Exception("Could not translate configuration");
+				}
+				
+				variantGenerator.createAndSaveVariantFromConfiguration(deConfiguration, variantFolder);
+				
+				// clean temporary files
+				for(EObject eObject : deltaEcoreModels) {
+					eObject.eResource().delete(null);
+				}
+				tempDeltaEcoreFolder.delete(true, null);
+				
+				
+				// Output message
 				IPath variantFolderPath = variantFolder.getFullPath();
-				MessageDialog.openInformation(shell, "Variant Creation Completed",
-						"Variant successfully created in \"" + variantFolderPath.toOSString() + "\".");
+				MessageDialog.openInformation(shell, "Variant Generation Completed", "Variant successfully generated in \"" + variantFolderPath.toOSString() + "\".");
 			}
-
-		} catch (Exception ex) {
-			// Debug
+			
+		} catch(Exception ex) {
+			//Debug
 			ex.printStackTrace();
-
-			String message = "Variant could not be derived:\n\n" + ex.getMessage();
-			MessageDialog.openError(shell, "Error", message);
+			
+			String message = "Variant could not be generated:\n\n" + ex.getMessage();
+			MessageDialog.openError(shell, failureMessageTitle, message);
 		}
 
+	}
+	
+	private List<EObject> getDeltaEcoreModels(HyFeatureModel featureModel, HyConfiguration configuration, HyMappingModel mapping, Date date) {
+		List<EObject> deltaEcoreModels = new ArrayList<EObject>(3);
+		
+		HFMExporter hfmExporter = new HFMExporter();
+		DEFeatureModel deFeatureModel;
+		try {
+			deFeatureModel = hfmExporter.exportFeatureModel(featureModel, date);
+			HFMConfigurationExporter configurationExporter = new HFMConfigurationExporter(hfmExporter.getFeatureMapping(), hfmExporter.getVersionMapping(), featureModel, deFeatureModel);
+			DEConfiguration deConfiguration = configurationExporter.exportConfiguration(configuration, date);
+			
+			
+			HFMMappingExporter mappingExporter = new HFMMappingExporter(hfmExporter.getFeatureMapping(), hfmExporter.getVersionMapping());
+			DEMappingModel deMapping = mappingExporter.exportMappingModel(mapping, date);
+			
+			saveTemporaryDeltaEcoreFiles(EcoreIOUtil.getFile(featureModel.eResource()).getProject(), featureModel.eResource().getResourceSet(), deFeatureModel, deMapping, deConfiguration);
+			
+			deltaEcoreModels.add(deConfiguration);
+			deltaEcoreModels.add(deMapping);
+			deltaEcoreModels.add(deFeatureModel);
+			
+			return deltaEcoreModels;
+		} catch (HyFeatureModelWellFormednessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return deltaEcoreModels;
+		
+	}
+	
+	
+	private void saveTemporaryDeltaEcoreFiles(IProject p, ResourceSet resourceSet, DEFeatureModel featureModel, DEMappingModel mapping, DEConfiguration configuration) {
+//		URI uriWithoutExtension = uri.trimFileExtension();
+		
+		if(!p.isOpen()) {
+			try {
+				p.open(null);
+			} catch (CoreException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		tempDeltaEcoreFolder = p.getFolder(".tempDeltaEcoreFiles");
+		
+		String baseFileName = "tempDeltaEcoreModel";
+		
+		IFile featureModelFile = tempDeltaEcoreFolder.getFile(baseFileName+"."+DEFeatureIOUtil.getDefaultFileExtension());
+		IFile mappingFile = tempDeltaEcoreFolder.getFile(baseFileName+"."+DEMappingIOUtil.getCurrentFileExtension());
+		IFile configurationFile = tempDeltaEcoreFolder.getFile(baseFileName+"."+DEConfigurationIOUtil.getCurrentFileExtension());
+		
+		EcoreIOUtil.saveModelAs(featureModel, featureModelFile, resourceSet);
+		EcoreIOUtil.saveModelAs(mapping, mappingFile, resourceSet);
+		EcoreIOUtil.saveModelAs(configuration, configurationFile, resourceSet);
 	}
 
 }
