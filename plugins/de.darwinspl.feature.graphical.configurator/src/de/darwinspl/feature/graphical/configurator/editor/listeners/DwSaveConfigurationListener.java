3e2cc26543c5a606d6708205b3881893019d9713
Michael Nieke <github@n1eke.de>
2017-09-20 11:45:17 +0200

    Saving configurations now also works for projects not located in
    the workspace root

--- plugins/de.darwinspl.feature.graphical.configurator/src/de/darwinspl/feature/graphical/configurator/editor/listeners/DwSaveConfigurationListener.java
+++ plugins/de.darwinspl.feature.graphical.configurator/src/de/darwinspl/feature/graphical/configurator/editor/listeners/DwSaveConfigurationListener.java
@@ -4,13 +4,15 @@ import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 
-import de.christophseidl.util.eclipse.ResourceUtil;
 import de.christophseidl.util.ecore.EcoreIOUtil;
 import de.darwinspl.feature.graphical.configurator.editor.DwFeatureModelConfiguratorEditor;
 import eu.hyvar.feature.HyFeatureModel;
@@ -52,9 +54,58 @@ public class DwSaveConfigurationListener extends SelectionAdapter {
 	    	return;
 	    }
 	    
-	    IFile configurationFile = ResourceUtil.makeFileRelativeToWorkspace(configurationAbsoluteFilePath);
+	    IProject project = featureModelFile.getProject();
+	    try {
+			project.refreshLocal(IResource.DEPTH_INFINITE, null);
+		} catch (CoreException e2) {
+			e2.printStackTrace();
+		}
+	    String projectFilePath = project.getLocation().toOSString();
 	    
-	    EcoreIOUtil.saveModelAs(configuration, configurationFile, featureModel.eResource().getResourceSet());
+	    if(configurationAbsoluteFilePath.startsWith(projectFilePath)) {
+	    	configurationAbsoluteFilePath = configurationAbsoluteFilePath.substring(projectFilePath.length()+1);
+	    	
+	    	String[] splits = configurationAbsoluteFilePath.split("\\\\");
+	    	IFolder lastFolder = null;
+	    	
+	    	String fileName = null;
+	    	
+	    	for(int i = 0; i<splits.length;i++) {
+	    		if(i == splits.length-1) {
+	    			fileName = splits[i];
+	    		}
+	    		else {
+	    			if(lastFolder != null) {
+	    				lastFolder = lastFolder.getFolder(splits[i]);
+	    			}
+	    			else {
+	    				lastFolder = project.getFolder(splits[i]);
+	    			}
+	    			
+	    			if(!lastFolder.exists()) {
+	    				try {
+							lastFolder.create(true, true, null);
+						} catch (CoreException e1) {
+							e1.printStackTrace();
+						}
+	    			}
+	    		}
+	    	}
+	    	
+	    	IFile configurationFile = null;
+	    	
+	    	if(lastFolder == null) {
+	    		configurationFile = project.getFile(fileName);
+	    	}
+	    	else {
+	    		 configurationFile = lastFolder.getFile(fileName);
+	    	}
+	    	
+		    EcoreIOUtil.saveModelAs(configuration, configurationFile, featureModel.eResource().getResourceSet());
+	    }
+	    else {
+	    	MessageDialog.openError(shell, "Could not save configuration", "Configurations need to be saved in the same project as the feature model");
+	    }
 	}
 	
 }
