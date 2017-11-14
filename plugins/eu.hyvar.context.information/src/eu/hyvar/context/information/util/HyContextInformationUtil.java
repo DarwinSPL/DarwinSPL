8d954675c999d7aa68b94454644aff47d42c0cec
Michael Nieke <github@n1eke.de>
2017-11-14 11:57:20 +0100

    changed file extensions to be consistent

--- plugins/eu.hyvar.context.information/src/eu/hyvar/context/information/util/HyContextInformationUtil.java
+++ plugins/eu.hyvar.context.information/src/eu/hyvar/context/information/util/HyContextInformationUtil.java
@@ -2,7 +2,7 @@ package eu.hyvar.context.information.util;
 
 public class HyContextInformationUtil {
 
-	private static final String CONTEXT_MODEL_FILE_EXTENSION_FOR_XMI = "hycontextmodel";
+	private static final String CONTEXT_MODEL_FILE_EXTENSION_FOR_XMI = "hycontextinformation_xmi";
 	private static final String CONTEXT_MODEL_FILE_EXTENSION_FOR_CONCRETE_SYNTAX = "hycontextinformation";
 	
 	public static String getContextModelFileExtensionForXmi() {
--- plugins/eu.hyvar.context.validity_formulas/src/eu/hyvar/context/contextValidity/util/HyValidityModelUtil.java
+++ plugins/eu.hyvar.context.validity_formulas/src/eu/hyvar/context/contextValidity/util/HyValidityModelUtil.java
@@ -2,7 +2,7 @@ package eu.hyvar.context.contextValidity.util;
 
 public class HyValidityModelUtil {
 	
-	private static final String VALIDITY_MODEL_FILE_EXTENSION_FOR_XMI = "hyvaliditymodel";
+	private static final String VALIDITY_MODEL_FILE_EXTENSION_FOR_XMI = "hyvalidityformula_xmi";
 	private static final String VALIDITY_MODEL_FILE_EXTENSION_FOR_CONCRETE_SYNTAX = "hyvalidityformula";
 	
 	public static String getValidityModelFileExtensionForXmi() {
