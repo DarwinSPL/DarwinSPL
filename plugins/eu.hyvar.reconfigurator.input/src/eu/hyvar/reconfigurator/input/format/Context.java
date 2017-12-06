6eade74c4c1f9521472dde2e3d2a806145ce2f0b
Michael Nieke <github@n1eke.de>
2017-12-06 14:32:06 +0100

    Removed annotations for OpenJDK 9 compatibility

--- plugins/eu.hyvar.reconfigurator.input/src/eu/hyvar/reconfigurator/input/format/Context.java
+++ plugins/eu.hyvar.reconfigurator.input/src/eu/hyvar/reconfigurator/input/format/Context.java
@@ -1,11 +1,11 @@
 
 package eu.hyvar.reconfigurator.input.format;
 
-import javax.annotation.Generated;
+//import javax.annotation.Generated;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-@Generated("org.jsonschema2pojo")
+//@Generated("org.jsonschema2pojo")
 public class Context {
 
     /**
