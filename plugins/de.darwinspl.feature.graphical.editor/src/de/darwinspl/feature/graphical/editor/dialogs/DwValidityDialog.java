8fc554450acfc929b60f100467bb01d47eba2b26
Daniel Röhrs <roehrsdaniel@gmail.com>
2018-01-31 22:29:40 +0100

    #73 Implemented validity check for HyNames

--- plugins/de.darwinspl.feature.graphical.editor/src/de/darwinspl/feature/graphical/editor/dialogs/DwValidityDialog.java
+++ plugins/de.darwinspl.feature.graphical.editor/src/de/darwinspl/feature/graphical/editor/dialogs/DwValidityDialog.java
@@ -2,6 +2,7 @@ package de.darwinspl.feature.graphical.editor.dialogs;
 
 import java.util.Date;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
@@ -12,41 +13,49 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
 import de.darwinspl.common.eclipse.ui.dialogs.DwDateDialog;
+import eu.hyvar.evolution.HyName;
+import eu.hyvar.evolution.HyNamedElement;
 import eu.hyvar.evolution.HyTemporalElement;
 
-public class DwValidityDialog extends DwSelectionDialog{
+public class DwValidityDialog extends DwSelectionDialog {
 	Text validSince;
 	Text validUntil;
+
 	public DwValidityDialog(Shell parentShell) {
 		super(parentShell);
 	}
-	
+
 	@Override
 	protected void createWidgets(Composite parent) {
 		Composite buttonContainer = new Composite(parent, SWT.NONE);
 		GridLayout layout = new GridLayout(2, false);
 		buttonContainer.setLayout(layout);
 
-	    
 		validSince = new Text(buttonContainer, SWT.READ_ONLY);
 		validSince.setText("Read Only");
-	    
-	    Button buttonSince = new Button(buttonContainer, SWT.PUSH);
-	    buttonSince.setText("Change Since");
-	    
-	    //buttonSince.setLayoutData(new RowData(100, 35));
-	    buttonSince.addSelectionListener(new SelectionListener(){
+
+		Button buttonSince = new Button(buttonContainer, SWT.PUSH);
+		buttonSince.setText("Change Since");
+
+		// buttonSince.setLayoutData(new RowData(100, 35));
+		buttonSince.addSelectionListener(new SelectionListener() {
 
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				if(element instanceof HyTemporalElement){
+				if (element instanceof HyTemporalElement) {
 					DwDateDialog dialog = new DwDateDialog(getShell(), convertStringToDate(validSince.getText()));
 					dialog.open();
-					if(dialog.getReturnCode() == OK){
+					if (dialog.getReturnCode() == OK) {
 						Date value = dialog.getValue();
-						
 						validSince.setText(getDateFormatted(value, DATE_FORMATTING));
-						((HyTemporalElement)element).setValidSince(value);
+						((HyTemporalElement) element).setValidSince(value);
+
+						EList<HyName> namedElements = ((HyNamedElement) element).getNames();
+						for (int index = 0; index <= namedElements.size(); index++) {
+							if (value.after(namedElements.get(index).getValidSince())) {
+								namedElements.get(index).setValidSince(value);
+							}
+						}
 					}
 				}
 			}
@@ -54,29 +63,29 @@ public class DwValidityDialog extends DwSelectionDialog{
 			@Override
 			public void widgetDefaultSelected(SelectionEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-	    	
-	    });
-	
-	    validUntil = new Text(buttonContainer, SWT.READ_ONLY);
-	    validUntil.setText("Read Only");
-	    
-	    Button buttonUntil = new Button(buttonContainer, SWT.PUSH);
-	    buttonUntil.setText("Change Until");
-	    //buttonUntil.setLayoutData(new RowData(100, 35));
-	    buttonUntil.addSelectionListener(new SelectionListener(){
+
+		});
+
+		validUntil = new Text(buttonContainer, SWT.READ_ONLY);
+		validUntil.setText("Read Only");
+
+		Button buttonUntil = new Button(buttonContainer, SWT.PUSH);
+		buttonUntil.setText("Change Until");
+		// buttonUntil.setLayoutData(new RowData(100, 35));
+		buttonUntil.addSelectionListener(new SelectionListener() {
 
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				if(element instanceof HyTemporalElement){
+				if (element instanceof HyTemporalElement) {
 					DwDateDialog dialog = new DwDateDialog(getShell(), convertStringToDate(validUntil.getText()));
 					dialog.open();
-					if(dialog.getReturnCode() == OK){
+					if (dialog.getReturnCode() == OK) {
 						Date value = dialog.getValue();
-						
+
 						validUntil.setText(getDateFormatted(value, DATE_FORMATTING));
-						((HyTemporalElement)element).setValidUntil(value);
+						((HyTemporalElement) element).setValidUntil(value);
 					}
 				}
 			}
@@ -84,27 +93,26 @@ public class DwValidityDialog extends DwSelectionDialog{
 			@Override
 			public void widgetDefaultSelected(SelectionEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-	    	
-	    });
-	    
-		HyTemporalElement temporalElement = (HyTemporalElement)element;
+
+		});
+
+		HyTemporalElement temporalElement = (HyTemporalElement) element;
 		validSince.setText(getDateFormatted(temporalElement.getValidSince(), DATE_FORMATTING));
 		validUntil.setText(getDateFormatted(temporalElement.getValidUntil(), DATE_FORMATTING));
 	}
-	
 
 	@Override
-	protected void okPressed(){
-		HyTemporalElement temporalElement = (HyTemporalElement)element;
+	protected void okPressed() {
+		HyTemporalElement temporalElement = (HyTemporalElement) element;
 		temporalElement.setValidSince(convertStringToDate(validSince.getText()));
 		temporalElement.setValidUntil(convertStringToDate(validUntil.getText()));
 		super.okPressed();
 	}
-	
+
 	@Override
-	protected void cancelPressed(){
+	protected void cancelPressed() {
 
 		super.cancelPressed();
 	}
