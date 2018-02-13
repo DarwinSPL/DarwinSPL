8ea789f28d434e821275d190ad46684fefc79ffb
alahola <a.ahola@tu-bs.de>
2018-02-13 14:28:24 +0100

    Configuration Editor - Server Dialog

--- plugins/de.darwinspl.feature.graphical.configurator/src/de/darwinspl/feature/graphical/configurator/dialogs/DwRESTServerSelectDialog.java
+++ plugins/de.darwinspl.feature.graphical.configurator/src/de/darwinspl/feature/graphical/configurator/dialogs/DwRESTServerSelectDialog.java
@@ -1,5 +1,6 @@
 package de.darwinspl.feature.graphical.configurator.dialogs;
 
+
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.IMessageProvider;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
@@ -9,10 +10,13 @@ import org.eclipse.jface.fieldassist.FieldDecorationRegistry;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
@@ -20,12 +24,29 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
 public class DwRESTServerSelectDialog extends TitleAreaDialog{
+	
+	
 	private Text uriText;
+	
+	private Boolean httpAuthentificationEnabled;
+	public Boolean getHttpAuthentificationEnabled() {
+		return httpAuthentificationEnabled;
+	}
+
+
+
+
+	public void setHttpAuthentificationEnabled(Boolean httpAuthentificationEnabled) {
+		this.httpAuthentificationEnabled = httpAuthentificationEnabled;
+	}
+
 	private Text userNameText;
 	private Text passwordText;
-	private String uri;
-	private String userName;
-	private String password;
+	
+	
+	private String uri = null;
+	private String userName = null;
+	private String password = null;
 	
 	public String getUri() {
 		return uri;
@@ -48,10 +69,20 @@ public class DwRESTServerSelectDialog extends TitleAreaDialog{
 
 
 
-	public DwRESTServerSelectDialog(Shell parentShell, String uri) {
+	public DwRESTServerSelectDialog(Shell parentShell, String uri, Boolean isHttpAuthEnabled, String username, String password) {
 		super(parentShell);
 		
 		this.uri = uri;
+		if(username != null){
+			this.userName = username;
+		}
+		if(password != null){
+			this.password = password;
+		} 
+		if(isHttpAuthEnabled != null){
+			this.httpAuthentificationEnabled = isHttpAuthEnabled;
+		}
+		
 	}
 
 
@@ -72,7 +103,7 @@ public class DwRESTServerSelectDialog extends TitleAreaDialog{
 		Composite area = (Composite) super.createDialogArea(parent);
 		Composite container = new Composite(area, SWT.NONE);
 		container.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		GridLayout layout = new GridLayout(3, false);
+		GridLayout layout = new GridLayout(2, false);
 		container.setLayout(layout);
 
 		Label uriLabel = new Label(container, SWT.NONE);
@@ -86,20 +117,107 @@ public class DwRESTServerSelectDialog extends TitleAreaDialog{
 		uriText = new Text(container, SWT.BORDER);
 		uriText.setLayoutData(dataURI);
 		uriText.setText(uri.toString());
+
+		
+		GridData horizontalFillGridData = new GridData(SWT.FILL, SWT.TOP, true, false, 2, 1);
+     
+		
+	
+		Button enableAuthCheckBox = new Button(container, SWT.CHECK);
+		enableAuthCheckBox.setText("Enable HTTP Authentification");
+		enableAuthCheckBox.setTextDirection(SWT.LEFT);
+		enableAuthCheckBox.setLayoutData(horizontalFillGridData);
+		
 		
 		Label usernameLabel = new Label(container, SWT.NONE);
 		usernameLabel.setText("Username:");
 		
 		userNameText = new Text(container, SWT.BORDER);
 		userNameText.setLayoutData(dataURI);
-		userNameText.setText("");
+		
+		if(userName != null){
+			userNameText.setText(userName);
+		} else{
+			userNameText.setText("");
+		}
+		
+
+		
 		
 		Label passwordLabel = new Label(container, SWT.NONE);
 		passwordLabel.setText("Password:");
 		
 		passwordText = new Text(container, SWT.BORDER);
 		passwordText.setLayoutData(dataURI);
+		passwordText.setEchoChar('*');
+		if(password != null){
+			passwordText.setText(password);
+		}else{
 		passwordText.setText("");
+		}
+		
+		if(httpAuthentificationEnabled!=null){
+			enableAuthCheckBox.setSelection(httpAuthentificationEnabled);
+			passwordText.setEnabled(httpAuthentificationEnabled);
+			userNameText.setEnabled(httpAuthentificationEnabled);
+		} else{
+			enableAuthCheckBox.setSelection(false);
+			passwordText.setEnabled(false);
+			userNameText.setEnabled(false);
+		
+		}
+		
+		enableAuthCheckBox.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+		
+				Button source = (Button) e.getSource();
+				Boolean isSelected = source.getSelection();
+				userNameText.setEnabled(isSelected);
+				passwordText.setEnabled(isSelected);
+				httpAuthentificationEnabled = isSelected;
+				
+				
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+
+		
+		
+		Button showPassword = new Button(container, SWT.CHECK);
+		showPassword.setText("Show Password");
+		showPassword.setTextDirection(SWT.LEFT);
+		showPassword.setLayoutData(horizontalFillGridData);
+		
+		showPassword.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				Button source = (Button) e.getSource();
+				Boolean isSelected = source.getSelection();
+				if(isSelected){
+				passwordText.setEchoChar('\0');
+				passwordText.redraw();
+				}else{
+					passwordText.setEchoChar('*');
+					passwordText.redraw();
+				}
+				
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
 		
 		
 		final ControlDecoration txtDecorator = new ControlDecoration(uriText, SWT.TOP|SWT.RIGHT|SWT.FILL);
