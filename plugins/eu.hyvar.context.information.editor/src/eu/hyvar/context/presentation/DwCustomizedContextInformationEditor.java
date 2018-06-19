d285abf51a23fd9e748a60c2a1f2c679aa496534
Daniel Röhrs <roehrsdaniel@gmail.com>
2018-06-19 19:01:40 +0200

    Date slider updated with model dates

--- plugins/eu.hyvar.context.information.editor/src/eu/hyvar/context/presentation/DwCustomizedContextInformationEditor.java
+++ plugins/eu.hyvar.context.information.editor/src/eu/hyvar/context/presentation/DwCustomizedContextInformationEditor.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import java.util.EventObject;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
@@ -18,6 +20,8 @@ import org.eclipse.swt.layout.RowData;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Scale;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IEditorInput;
@@ -29,8 +33,9 @@ import org.eclipse.ui.dialogs.SaveAsDialog;
 import org.eclipse.ui.part.EditorPart;
 import org.eclipse.ui.part.FileEditorInput;
 
-import eu.hyvar.context.HyContextualInformation;
 import eu.hyvar.context.util.HyContextInformationAdapterFactory;
+import eu.hyvar.evolution.util.HyEvolutionUtil;
+
 import org.eclipse.emf.common.command.BasicCommandStack;
 import org.eclipse.emf.common.command.CommandStackListener;
 import org.eclipse.emf.common.util.EList;
@@ -38,6 +43,7 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecp.ui.view.ECPRendererException;
+import org.eclipse.emf.ecp.ui.view.swt.ECPSWTView;
 import org.eclipse.emf.ecp.ui.view.swt.ECPSWTViewRenderer;
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
@@ -45,7 +51,7 @@ import org.eclipse.emf.edit.ui.action.EditingDomainActionBarContributor;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 
-public class DwCustomizedContextInformationEditor extends EditorPart {
+public class DwCustomizedContextInformationEditor extends EditorPart implements Listener {
 
 	protected AdapterFactoryEditingDomain editingDomain;
 	protected ComposedAdapterFactory adapterFactory;
@@ -53,12 +59,13 @@ public class DwCustomizedContextInformationEditor extends EditorPart {
 
 	List<Date> dates;
 	Date currentSelectedDate;
+	boolean lastDateSelected;
 
 	// UI Elements
 	Composite buttonGroup;
-	Button currentDate;
-	Button addDate;
+	Button currentDateButton;
 	Scale scale;
+	private ECPSWTView renderedComposite;
 
 	public DwCustomizedContextInformationEditor() {
 		super();
@@ -70,6 +77,9 @@ public class DwCustomizedContextInformationEditor extends EditorPart {
 			contextResource.save(null);
 			((BasicCommandStack) editingDomain.getCommandStack()).saveIsDone();
 			firePropertyChange(IEditorPart.PROP_DIRTY);
+			
+			//this forces a new date list to be fetched
+			refreshDatesList();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -104,6 +114,8 @@ public class DwCustomizedContextInformationEditor extends EditorPart {
 		setSite(site);
 		setInput(input);
 		setPartName(input.getName());
+		// initialize the date list
+		dates = new ArrayList<Date>();
 		if (input instanceof IFileEditorInput) {
 			IFileEditorInput fileInput = (IFileEditorInput) input;
 			IFile file = fileInput.getFile();
@@ -143,9 +155,10 @@ public class DwCustomizedContextInformationEditor extends EditorPart {
 			content.setLayout(GridLayoutFactory.fillDefaults().margins(10, 10).create());
 			content.setLayoutData(GridDataFactory.fillDefaults().create());
 			EObject contextObject = contextResource.getContents().get(0);
-			ECPSWTViewRenderer.INSTANCE.render(content, contextObject);
+			renderedComposite = ECPSWTViewRenderer.INSTANCE.render(content, contextObject);
 
 			createSliderControl(content);
+			registerControlListeners();
 
 		} catch (ECPRendererException e) {
 			e.printStackTrace();
@@ -155,56 +168,27 @@ public class DwCustomizedContextInformationEditor extends EditorPart {
 	}
 
 	private void createSliderControl(Composite parent) {
-		EList<EObject> contents = contextResource.getContents().get(0).eContents();
-		dates = new ArrayList<Date>();
-		for (EObject obj : contents) {
-			if (obj instanceof HyContextualInformation) {
-				Date x = ((HyContextualInformation) obj).getValidSince();
-				if(!dates.contains(x))
-				dates.add(x);
-			}
-		}
+		refreshDatesList();
 
-		if (dates.isEmpty())
-			dates.add(new Date());
 		int size = dates.size();
-		if (size == 1 && dates.get(0) == null) {
-			dates.clear();
-			dates.add(new Date());
-		}
+
 		buttonGroup = new Composite(parent, SWT.NONE);
 		RowLayout rowLayout = new RowLayout(SWT.HORIZONTAL);
 		rowLayout.justify = true;
+		rowLayout.pack = true;
 		buttonGroup.setLayout(rowLayout);
 
-		currentDate = new Button(buttonGroup, SWT.PUSH);
-
-		if (size > 0)
-			currentDate.setText(dates.get(0).toString());
-		else {
-			currentDate.setText((new Date()).toString());
-		}
+		currentDateButton = new Button(buttonGroup, SWT.PUSH);
 
 		scale = new Scale(buttonGroup, SWT.FILL);
 		scale.setMinimum(0);
 		scale.setMaximum(size - 1);
 		scale.setLayoutData(new RowData(300, SWT.DEFAULT));
 		scale.setEnabled(size > 1);
-		scale.setSelection(0);
+		scale.setSelection(dates.indexOf(currentSelectedDate));
 
-		addDate = new Button(buttonGroup, SWT.PUSH);
-		addDate.setText("Add Date");
-
-		// resetDates = new Button(buttonGroup, SWT.PUSH);
-		// resetDates.setText("Reset Dates");
-
-		if (dates.size() > 0)
-			setCurrentSelectedDate(currentSelectedDate);
-		else {
-			Date now = new Date();
-			dates.add(now);
-			setCurrentSelectedDate(now);
-		}
+		setCurrentSelectedDate(currentSelectedDate);
+		buttonGroup.pack();
 
 		/**
 		 * register control listener for visualization bug if a side editor was added
@@ -225,13 +209,29 @@ public class DwCustomizedContextInformationEditor extends EditorPart {
 	}
 
 	private void setCurrentSelectedDate(Date date) {
-		if (dates.contains(date))
+		// is the date in the list?
+		if (dates.contains(date)) {
 			this.currentSelectedDate = date;
+
+			// is it since forever?
+			if (currentSelectedDate.getTime() == Long.MIN_VALUE) {
+				if (dates.size() > 1)
+					currentDateButton.setText("before " + dates.get(1).toString());
+				// is it until forever?
+			} else if (currentSelectedDate.getTime() == Long.MAX_VALUE) {
+				currentDateButton.setText("after " + dates.get(dates.size() - 2).toString());
+			} else {
+				currentDateButton.setText(currentSelectedDate.toString());
+			}
+			scale.setSelection(dates.indexOf(currentSelectedDate));
+		}
 	}
 
 	protected void loadContent(IFile file) throws IOException {
+		// initiate editing domain with new command stack
 		editingDomain = new AdapterFactoryEditingDomain(new HyContextInformationAdapterFactory(),
 				new BasicCommandStack());
+		// load the file into the resource
 		contextResource = editingDomain.createResource(file.getFullPath().toString());
 		contextResource.load(null);
 	}
@@ -248,6 +248,60 @@ public class DwCustomizedContextInformationEditor extends EditorPart {
 
 	public EditingDomainActionBarContributor getActionBarContributor() {
 		return (EditingDomainActionBarContributor) getEditorSite().getActionBarContributor();
+	}
+
+	public void registerControlListeners() {
+		// Left button to select an individual date
+		currentDateButton.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event) {
+				Object[] datePick = dates.toArray();
+				Object value = JOptionPane.showInputDialog(null, "Choose a date", "Date", JOptionPane.QUESTION_MESSAGE,
+						null, datePick, datePick[0]);
+				int index = dates.indexOf(value);
+				setCurrentSelectedDate(dates.get(index));
+
+				int size = dates.size();
+				scale.setMaximum(size - 1);
+				scale.setEnabled(size > 1);
+			}
+		});
+
+		// Slider to select a given date
+		scale.addListener(SWT.Selection, this);
+	}
+
+	@Override
+	public void handleEvent(Event event) {
+		if (event.widget.equals(scale)) {
+			int index = scale.getSelection();
+			currentSelectedDate = dates.get(scale.getSelection());
+			setCurrentSelectedDate(currentSelectedDate);
+
+			if (index == dates.size() - 1)
+				lastDateSelected = true;
+		}
+	}
+
+	private void refreshDatesList() {
+		EList<EObject> contents = contextResource.getContents();
+		dates = HyEvolutionUtil.collectDates(contents.get(0));
+
+		if (dates.isEmpty()) {
+			dates.add(new Date(0));
+			dates.add(new Date());
+			dates.add(new Date(Long.MAX_VALUE));
+		} else if (dates.get(0).getTime() != Long.MIN_VALUE) {
+			dates.add(0, new Date(0));
+		} else if (dates.get(dates.size() - 1).getTime() != Long.MAX_VALUE) {
+			dates.add(new Date(Long.MAX_VALUE));
+		}
+
+		if (scale != null) {
+			int size = dates.size();
+			scale.setMaximum(size - 1);
+			scale.setEnabled(size > 1);
+			scale.getParent().pack();
+		}
 	}
 
 }
